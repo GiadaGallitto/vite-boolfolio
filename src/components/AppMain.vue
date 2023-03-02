aaef3dafc375f018cdfa21b707b20100e0d15e7f
GiadaGallitto <gallitto.giada@gmail.com>
2023-03-02 15:59:16 +0100

    Add Main component and scss

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+  name: 'AppMain',
+  data() {
+    return {
+      
+    }
+  },
+  
+  methods: {
+    
+  },
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
