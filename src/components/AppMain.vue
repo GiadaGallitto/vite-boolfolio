4c9d647352c175ee82a2ca1910676be483a4b18e
GiadaGallitto <gallitto.giada@gmail.com>
2023-03-02 16:42:13 +0100

    Create card and add types/technolog

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -43,11 +43,34 @@ export default {
     <div class="row justify-content-between g-4">
       <div class="col-6 project" v-for="project in projects">
         <div class="card">
+          <div class="card-header d-flex align-items-center justify-content-center mb-2">
+            <span>
+              <strong>Author: </strong>{{ project.author }}
+            </span>
+          </div>
+
+          <div class="d-flex justify-content-between mb-1">
+            <div class="type">
+              <p>
+                <strong>Type: </strong>{{ project.type.name }}
+              </p>
+            </div>
+            <div class="technologies">
+              <strong>Technology: </strong>
+              <span class="technology me-2" v-for="technology in project.technologies">
+                  {{ technology.name }}
+              </span>
+            </div>
+          </div>
+
           <h5 class="card-title">{{ project.title }}</h5>
           <img :src="project.image" class="img-fluid" alt="project-image">
           <div class="card-body">
             <p class="card-text">{{ project.description }}</p>
             <a href="#" class="btn btn-primary">Read more...</a>
+          </div>
+          <div class="card-footer text-muted">
+            <span><strong>Start date: </strong>{{ project.start_date }}</span>
           </div>
         </div>
       </div>
